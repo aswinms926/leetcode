f5689fc5bbed535120b073841fe9c776682d02b7
Aswin M.S <125258048+aswinms926@users.noreply.github.com>
2024-09-25 23:10:18 +0530

    Time: 323 ms (12.25%), Space: 12.7 MB (71.86%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        newstr=""
+        for c in s:
+            if c.isalnum():
+                newstr+=c.lower()
+        return newstr == newstr[::-1]        
+        
+        
